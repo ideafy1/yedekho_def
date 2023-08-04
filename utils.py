b30ca7948fd7368fbadf78163e697bb797b48aa4
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2023-08-04 16:42:16 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -397,11 +397,11 @@ async def get_shortlink(link):
                     return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://api.shareus.io/easy_api?key={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://api.shareus.io/direct_link?api_key={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://api.shareus.io/easy_api?key={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'https://api.shareus.io/direct_link?api_key={URL_SHORTNER_WEBSITE_API}&link={link}'
 # _______________________________________________________________________________________________________________ #
 # __________________________________________Credit_______________________________________________________________ #
 # _______________________________________LazyDeveloper___________________________________________________________ #
